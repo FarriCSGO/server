5c269bfa2cb4026877bcfa1b6ee34892429abff6
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-02 00:11:25 +0530

    Write some throw new Error statements

--- src/controllers/userSteamDetails.ts
+++ src/controllers/userSteamDetails.ts
@@ -10,8 +10,7 @@ class userSteamDetailsController {
     next: NextFunction
   ): Promise<any> {
     /* Declare an object `userSteamDetails` of type `USER_STEAM_DETAILS`
-     * which will be returned as JSON data back to the client.
-     * steam_level - user's steam profile level */
+     * which will be returned as JSON data back to the client */
     let userSteamDetails: USER_STEAM_DETAILS = {
       name: 'Error: data missing',
       steam_level: 'Error: data missing',
@@ -22,7 +21,7 @@ class userSteamDetailsController {
 
     try {
       // Fetch and set user's steam profile details
-      userSteamDetails = await actions.getUserSteamDetails(
+      userSteamDetails = await actions.STEAM.getUserSteamDetails(
         req.params.steam_id_64
       );
 
@@ -30,6 +29,8 @@ class userSteamDetailsController {
       if (!userSteamDetails) {
         throw Error;
       }
+
+      // Send client the JSON data
       res.json(userSteamDetails);
     } catch (error) {
       return next({
@@ -46,7 +47,7 @@ class userSteamDetailsController {
   ): Promise<any> {
     /* Declare an object `userSteamDetails` of type `USER_STEAM_DETAILS`
      * which will be returned as JSON data back to the client.
-     * steam_level - user's steam profile level */
+     */
     let userSteamDetails: USER_STEAM_DETAILS = {
       name: 'Error: data missing',
       steam_level: 'Error: data missing',
@@ -58,16 +59,27 @@ class userSteamDetailsController {
     let steam_id_64 = '';
 
     // Fetch user's steam_id_64 to get user's steam profile details
-    steam_id_64 = await actions.getSteamID64(req.params.profile_custom_url);
+    try {
+      steam_id_64 = await actions.STEAM.getSteamID64(
+        req.params.profile_custom_url
+      );
+    } catch (err) {
+      return next({
+        code: 404,
+        message: 'User does not exist according to STEAM WEB API'
+      });
+    }
 
     try {
       // Fetch and set user's steam profile details
-      userSteamDetails = await actions.getUserSteamDetails(steam_id_64);
+      userSteamDetails = await actions.STEAM.getUserSteamDetails(steam_id_64);
 
       // Throw an error if user does not exist in the steam's data base
       if (!userSteamDetails) {
         throw Error;
       }
+
+      // Send client the JSON data
       res.json(userSteamDetails);
     } catch (error) {
       return next({
