e5102cb6c5ddd716af5c0bf99ea67a8bf06e0d57
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-14 02:09:04 +0530

    Add Mongoose and connect to MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import { app } from "./app";
+import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const MONGO_DB_URL: string | any = process.env.MONGO_DB_URL;
+const PORT: string | number = process.env.PORT || 8080;
 
-// Start server
+mongoose
+  .connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.info("Connected to MongoDB"))
+  .catch((error) => console.error(error));
 
 app.listen(PORT, () =>
   console.info(`Server started at http://localhost:${PORT}`)
