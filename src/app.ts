20935aa6801e4a80aecae81fe6e17e947cc1e7c5
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-14 22:42:57 +0530

    Enable CORS to allow client to make API requests

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,22 @@ app.use(express.urlencoded({ extended: true }));
 // Log every request to the console that was sent to the server
 app.use(requestLogger);
 
+// Enabling CORS to allow client(deployed) to make API request
+let ORIGIN: string;
+
+process.env.NODE_ENV === "production"
+  ? (ORIGIN = "https://farri.netlify.app")
+  : (ORIGIN = "http://localhost:3000");
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", ORIGIN);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Route the request correctly based on the end-point
 app.use(routes);
 
