ffa9d5815f494b3e538407da94b03a7f5f98c426
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-14 11:27:51 +0530

    Remove MongoDB connection and Mongoose because it iss not needed at the moment

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
 import { app } from "./app";
-import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_DB_URL: string | any = process.env.MONGO_DB_URL;
 const PORT: string | number = process.env.PORT || 8080;
-
-mongoose
-  .connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.info("Connected to MongoDB"))
-  .catch((error) => console.error(error));
 
 app.listen(PORT, () =>
   console.info(`Server started at http://localhost:${PORT}`)
