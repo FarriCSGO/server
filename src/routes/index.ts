2d928dcfbd63a6dfad17369e4e132d3167fcd9b1
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-15 01:53:16 +0530

    Remove /api/quotesByPros

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,9 @@ import express from "express";
 
 // Route handlers
 import userSteamDetailsRouter from "./api/userSteamDetails";
-import quotesByProsRouter from "./api/quotesByPros";
 
 const router = express.Router();
 
 router.use("/api/userSteamDetails/", userSteamDetailsRouter);
-router.use("/api/quotesByPros", quotesByProsRouter);
 
 export default router;
