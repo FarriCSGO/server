97fb7e770988ac56e3d95447bfeac89a63be63fc
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-15 23:09:49 +0530

    Add route /api/steamID64

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export { default as IUserSteamDetails } from "./IUserSteamDetails";
 export { default as IError } from "./IError";
+export { default as ISteamID64 } from "./ISteamID64";
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 
 // Route handlers
 import userSteamDetailsRouter from "./api/userSteamDetails";
+import steamID64Router from "./api/steamID64";
 
 const router = express.Router();
 
 router.use("/api/userSteamDetails/", userSteamDetailsRouter);
+router.use("/api/steamID64/", steamID64Router);
 
 export default router;
