b72a96846c24693c51a38cc7b0da9d6f362da886
Shikhar Sharma <ceoshikhar@gmail.com>
2020-08-14 01:35:53 +0530

    CSGO NODE INTEGRATION

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Response, Request, NextFunction, IRouter } from "express";
+import { csgoClient } from "../server";
 
 // Route handlers
 import userSteamDetailsRouter from "./api/userSteamDetails.router";
@@ -10,5 +11,33 @@ const router = express.Router();
 router.use("/api/userSteamDetails/", userSteamDetailsRouter);
 router.use("/api/steamID64/", steamID64Router);
 router.use("/api/quickStats", quickStatsRouter);
+
+router.get("/api/csgo", (req: Request, res: Response, next: NextFunction) => {
+  console.log("WORKING");
+
+  const matchID = "3427254290832425251";
+  const outcomeID = "3427259489890337070";
+  const tokenID = 57904;
+
+  let data;
+
+  csgoClient.launch();
+  // TODO: Launch csgoClient in server.ts and have ready event handler here
+
+  csgoClient.on("ready", function test() {
+    console.log("LOL");
+    csgoClient.requestGame(matchID, outcomeID, tokenID);
+    csgoClient.on("matchList", function lol(resp) {
+      console.log(resp);
+      data = resp;
+      res.json(data);
+      csgoClient.removeListener("matchList", lol);
+    });
+
+    csgoClient.exit();
+    console.log("LOL AGAIN");
+    csgoClient.removeListener("ready", test);
+  });
+});
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,36 @@
 import { app } from "./app";
 import * as dotenv from "dotenv";
+import Steam = require("steam");
+import csgo = require("csgo");
 
 dotenv.config();
 
+const steamClient = new Steam.SteamClient();
+const steamUser = new Steam.SteamUser(steamClient);
+const steamGC = new Steam.SteamGameCoordinator(steamClient, 730);
+
+export const csgoClient = new csgo.CSGOClient(steamUser, steamGC, true);
+
 const PORT: string | number = process.env.PORT || 8080;
 
-app.listen(PORT, () =>
-  console.info(`Server started at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.info(`Server started at http://localhost:${PORT}`);
+
+  steamClient.connect();
+  steamClient.on("connected", function () {
+    steamUser.logOn({
+      account_name: "nitroxbot",
+      password: "krishna@1234"
+    });
+  });
+
+  steamClient.on("logOnResponse", (response) => {
+    if (response.eresult === Steam.EResult.OK) {
+      console.log("Login success.");
+    } else {
+      console.log("Login failure.");
+      console.log(response);
+      process.exit(1);
+    }
+  });
+});
